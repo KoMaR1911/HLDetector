d0079a721f4615d73659c368d2fe86e45f10b32e
Komar Komarowski <galozaur@gmail.com>
2021-07-11 22:10:14 +0200

    First Commit

--- HLDetector/HLDetector.cpp
+++ HLDetector/HLDetector.cpp
@@ -1,20 +1,93 @@
-﻿// HLDetector.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿
+#include <Windows.h>
 #include <iostream>
+#include "XOR.h"
+#include "Lazy_Import.h"
+#include "detours.h"
+
+#pragma comment(lib, "detours.lib")
+
+typedef struct _FILE_STANDARD_INFORMATION {
+	LARGE_INTEGER AllocationSize;
+	LARGE_INTEGER EndOfFile;
+	ULONG	      NumberOfLinks;
+	BOOLEAN	      DeletePending;
+	BOOLEAN	      Directory;
+} FILE_STANDARD_INFORMATION, * PFILE_STANDARD_INFORMATION;
+
+typedef struct _UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _OBJECT_ATTRIBUTES {
+	ULONG Length;
+	HANDLE RootDirectory;
+	PUNICODE_STRING ObjectName;
+	ULONG Attributes;
+	PVOID SecurityDescriptor;
+	PVOID SecurityQualityOfService;
+} OBJECT_ATTRIBUTES;
+typedef OBJECT_ATTRIBUTES* POBJECT_ATTRIBUTES;
+
+typedef struct _IO_STATUS_BLOCK
+{
+	union
+	{
+		NTSTATUS Status;
+		PVOID Pointer;
+	};
+	ULONG_PTR Information;
+} IO_STATUS_BLOCK, * PIO_STATUS_BLOCK;
+
+typedef NTSTATUS(NTAPI* fNtOpenFile)(PHANDLE FileHandle, ACCESS_MASK DesiredAccess, POBJECT_ATTRIBUTES ObjectAttributes, PIO_STATUS_BLOCK IoStatusBlock, ULONG ShareAccess, ULONG OpenOptions);
+
+fNtOpenFile nfNtOpenFile;
+
+NTSTATUS NTAPI hkNtOpenFile(PHANDLE FileHandle, ACCESS_MASK DesiredAccess, POBJECT_ATTRIBUTES ObjectAttributes, PIO_STATUS_BLOCK IoStatusBlock, ULONG ShareAccess, ULONG OpenOptions)
+{
+
+	auto STATUS = nfNtOpenFile(FileHandle, DesiredAccess, ObjectAttributes, IoStatusBlock, ShareAccess, OpenOptions);
+
+	if (ObjectAttributes != nullptr)
+	{
+		if (ObjectAttributes->ObjectName != nullptr && ObjectAttributes->ObjectName->Buffer != nullptr)
+		{
+			std::wstring a = XorStringW(L"\\data\\images\\");
+			std::wstring b = XorStringW(L"\\data\\config\\");
+			std::wstring c = XorStringW(L"HLBot");
+
+			//	printf("Hook NtOpenFile: %ws \n", ObjectAttributes->ObjectName->Buffer);
+
+			if ((std::wcsstr(ObjectAttributes->ObjectName->Buffer, a.c_str())) || (std::wcsstr(ObjectAttributes->ObjectName->Buffer, b.c_str()) || (std::wcsstr(ObjectAttributes->ObjectName->Buffer, c.c_str()))))
+			{
+				LI_FN(ExitProcess)(0);
+				LI_FN(__fastfail)(0);
+			}
+		}
+
+	}
+
+	return STATUS;
+
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	auto oNtOpenFile = fNtOpenFile(GetProcAddress(GetModuleHandleA(XorString("ntdll.dll")), XorString("NtOpenFile")));
+
+#ifdef DEBUG
+	std::cout << "NtOpenFile offset: " << oNtOpenFile << "\n";
+#endif // DEBUG
+
+
+
+	nfNtOpenFile = (fNtOpenFile)DetourFunction((PBYTE)oNtOpenFile, (PBYTE)hkNtOpenFile);
+
+#ifdef DEBUG
+	std::cout << "NtOpenFile hooked! \n ";
+#endif // DEBUG
+ 
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
